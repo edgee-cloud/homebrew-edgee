07f35e40794b5c58cfd04bad5d0be40fd3245a2b
Clement Bouvet <clement@cbouvet.fr>
2025-02-06 13:35:24 +0100

    add edgee formula

---
+++ edgee.rb
@@ -0,0 +1,15 @@
+class Edgee < Formula
+  desc "The full-stack edge platform for your edge-oriented applications."
+  homepage "https://github.com/edgee-cloud/edgee"
+  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v0.7.4.tar.gz"
+  sha256 "c16ca8d18a8a5263307ad9a913f6200c6566ad00152a682256423457e2163ce5"
+  license "Apache-2.0"
+  head "https://github.com/edgee-cloud/edgee.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "build", "-p", "edgee", "--bin", "edgee", "--release"
+    bin.install "target/release/edgee"
+  end
+end

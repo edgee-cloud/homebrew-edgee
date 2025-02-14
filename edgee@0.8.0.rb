446be62d9a5ee2626b709aa5c63e0e558ee81a6c
Alex Casalboni <alex@edgee.cloud>
2025-02-14 17:54:39 +0100

    add v0.8.1 (#2)

--- edgee@0.8.0.rb
+++ edgee@0.8.0.rb
@@ -1,8 +1,8 @@
 class Edgee < Formula
   desc "The full-stack edge platform for your edge-oriented applications."
   homepage "https://github.com/edgee-cloud/edgee"
-  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v0.8.1.tar.gz"
-  sha256 "2c3341ef6564101eab6c07ce6a21c5bb3dee5b10805860522e1df8a489f70010"
+  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v0.8.0.tar.gz"
+  sha256 "77b3e579295fcbc150d6e325f34278dcd16be3d7c13cda0657015408f122c021"
   license "Apache-2.0"
   head "https://github.com/edgee-cloud/edgee.git", branch: "main"
 

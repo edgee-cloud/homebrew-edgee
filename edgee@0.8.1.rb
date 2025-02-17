2d2172038dff9c12b9d69a810a2abd2011e322c6
Clément B <clement@edgee.cloud>
2025-02-17 13:53:46 +0100

    update to 0.8.2 (#3)

--- edgee@0.8.1.rb
+++ edgee@0.8.1.rb
@@ -1,8 +1,8 @@
 class Edgee < Formula
   desc "The full-stack edge platform for your edge-oriented applications."
   homepage "https://github.com/edgee-cloud/edgee"
-  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v0.8.2.tar.gz"
-  sha256 "d841a127154309bcf2cf976c626980824e646eab3fc0c8b922447294d1ad8265"
+  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v0.8.1.tar.gz"
+  sha256 "2c3341ef6564101eab6c07ce6a21c5bb3dee5b10805860522e1df8a489f70010"
   license "Apache-2.0"
   head "https://github.com/edgee-cloud/edgee.git", branch: "main"
 

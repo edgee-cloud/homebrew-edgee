b8a7dd9a73ddcf5b34542d1e0ee8210439c6213f
Alex Casalboni <alex@edgee.cloud>
2025-02-27 16:46:03 +0100

    Release v0.8.6, v0.8.5, and fix older file for 0.8.2 (#6)
    * rename 0.8.2 file
    
    * 0.8.5 release update
    
    * 0.8.6 release

--- edgee@0.8.4.rb
+++ edgee@0.8.4.rb
@@ -1,8 +1,8 @@
 class Edgee < Formula
   desc "The full-stack edge platform for your edge-oriented applications."
   homepage "https://github.com/edgee-cloud/edgee"
-  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v0.8.6.tar.gz"
-  sha256 "42ae86c4394cb7befa285a8ea3c2ed841432b6ddbff2382cda0af721ca448f1c"
+  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v0.8.4.tar.gz"
+  sha256 "a1b7840dddf10cdaa46e228f83c0162337f236daa1d26985686eeab8faaa9323"
   license "Apache-2.0"
   head "https://github.com/edgee-cloud/edgee.git", branch: "main"
 

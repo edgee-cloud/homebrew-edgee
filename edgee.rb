df9cb43892c8b15b255bc485bf46bf746bdebddd
Alex Casalboni <alex@edgee.cloud>
2025-04-16 15:18:11 +0200

    touch empty file to test commit and push

--- edgee.rb
+++ edgee.rb
@@ -2,7 +2,7 @@ class Edgee < Formula
   desc "The full-stack edge platform for your edge-oriented applications."
   homepage "https://github.com/edgee-cloud/edgee"
   url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v1.1.2.tar.gz"
-  sha256 "dcca4390ebad15925d5c80bd6be96b4a394067b1f99801ee59e5b2aa521ef1a"
+  sha256 "dcca4390ebad15925d5c80bd6be96b4a394067b1f99801ee59e5b2aa521ef1a8"
   license "Apache-2.0"
   head "https://github.com/edgee-cloud/edgee.git", branch: "main"
 

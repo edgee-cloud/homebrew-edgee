a5b616c0531e1c33aecebf7367db02e2fd758454
Alex Casalboni <alex@edgee.cloud>
2025-04-16 16:23:29 +0200

    GH action automation (#15)
    * update new-version script to handle git push and GH PR creation
    
    * define a new GH action to be used in Edgee CLI repo to update Homebrew on release
    
    * add test workflow to check if the action works (temporary)
    
    * passing GH_TOKEN as action input
    
    * typo
    
    * use
    
    * try again
    
    * typo
    
    * bash -x
    
    * no bash
    
    * chmod
    
    * shebang
    
    * inline the whole script, damn it
    
    * debugging
    
    * debugging
    
    * fix sed -i option for ubuntu
    
    * add git user email and user name
    
    * edit edgee.rb to test git commit and push
    
    * touch empty file to test commit and push
    
    * forgot git add
    
    * update script to fix sed
    
    * check where we are
    
    * restored action structure
    
    * typo
    
    * setting working directory, adding grouped logging
    
    * remove sed option var
    
    * add ::debug::
    
    * debugging
    
    * debugging
    
    * try bash -x again
    
    * checkout the temporary branch wtf
    
    * change branch name for tests
    
    * remove ::debug::
    
    * typo
    
    * remove -x
    
    * remove test workflow
    
    * reference main branch
    
    * remove test.txt file

--- new-version.sh
+++ new-version.sh
@@ -1,7 +1,48 @@
+# define previous/current release variables
 PREVIOUS_RELEASE=$(gh release list --repo edgee-cloud/edgee --limit 2 --json tagName --jq '.[1].tagName')
+PREVIOUS_RELEASE_WITHOUT_V=$(echo $PREVIOUS_RELEASE | sed -e "s/v//g")
 NEW_RELEASE=$(gh release list --repo edgee-cloud/edgee --limit 2 --json tagName --jq '.[0].tagName')
-wget https://github.com/edgee-cloud/edgee/archive/refs/tags/$NEW_RELEASE.tar.gz
-cp edgee.rb edgee@$PREVIOUS_RELEASE.rb
+
+# download the new release and previous release tarballs so we can calculate the sha256sum
+echo "Downloading latest release $NEW_RELEASE..."
+wget -nv  https://github.com/edgee-cloud/edgee/archive/refs/tags/$NEW_RELEASE.tar.gz
+echo "Downloading previous release $PREVIOUS_RELEASE..."
+wget -nv  https://github.com/edgee-cloud/edgee/archive/refs/tags/$PREVIOUS_RELEASE.tar.gz
+
+# calculate the sha256sum of the new release tarballs
 NEW_SHA256_SUM=$(sha256sum $NEW_RELEASE.tar.gz | cut -c1-64)
-sed -i '' -e "s|sha256 \".*\"|sha256 \"$NEW_SHA256_SUM\"|g" ./edgee.rb
-sed -i '' -e "s|url \".*\"|url \"https://github.com/edgee-cloud/edgee/archive/refs/tags/$NEW_RELEASE.tar.gz\"|g" ./edgee.rb
+PREVIOUS_SHA256_SUM=$(sha256sum $PREVIOUS_RELEASE.tar.gz | cut -c1-64)
+
+# create a new formula file for the previous release
+echo "Creating new formula file for $PREVIOUS_RELEASE..."
+cp edgee.rb edgee@$PREVIOUS_RELEASE_WITHOUT_V.rb
+
+# update previous release formula with the correct sha256sum
+# we use the re-computed SHA256 to make this script idempotent
+echo "Updating $PREVIOUS_RELEASE formula with the correct sha256sum..."
+sed -i -e "s|sha256 \".*\"|sha256 \"$PREVIOUS_SHA256_SUM\"|g" ./edgee@$PREVIOUS_RELEASE_WITHOUT_V.rb
+sed -i -e "s|url \".*\"|url \"https://github.com/edgee-cloud/edgee/archive/refs/tags/$PREVIOUS_RELEASE.tar.gz\"|g" ./edgee@$PREVIOUS_RELEASE_WITHOUT_V.rb
+
+# update edgee.rb with the latest
+echo "Updating edgee.rb with the $NEW_RELEASE sha256sum..."
+sed -i -e "s|sha256 \".*\"|sha256 \"$NEW_SHA256_SUM\"|g" ./edgee.rb
+sed -i -e "s|url \".*\"|url \"https://github.com/edgee-cloud/edgee/archive/refs/tags/$NEW_RELEASE.tar.gz\"|g" ./edgee.rb
+
+# clean up temporary gz files
+echo "Cleaning up temporary files..."
+rm $NEW_RELEASE.tar.gz
+rm $PREVIOUS_RELEASE.tar.gz
+
+# commit and push the changes to a new branch
+echo "Committing and pushing..."
+git config --global user.email "tech@edgee.cloud"
+git config --global user.name "Edgee team"
+NEW_BRANCH=edgee-release-$NEW_RELEASE
+git checkout -b $NEW_BRANCH
+git add edgee@$PREVIOUS_RELEASE_WITHOUT_V.rb
+git commit -a -m "Release $NEW_RELEASE"
+git push origin $NEW_BRANCH
+
+# create a pull request
+echo "Creating pull request..."
+gh pr create --base main --head $NEW_BRANCH --title "Release $NEW_RELEASE" --body "Release $NEW_RELEASE"

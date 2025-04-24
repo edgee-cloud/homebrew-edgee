86e6938256068470859dd60cf42427a14f6fb989
Clement Bouvet <clement@cbouvet.fr>
2025-04-24 12:00:14 +0200

    Release v1.2.1

--- edgee@1.2.0.rb
+++ edgee@1.2.0.rb
@@ -1,8 +1,8 @@
 class Edgee < Formula
   desc "The full-stack edge platform for your edge-oriented applications."
   homepage "https://github.com/edgee-cloud/edgee"
-  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v1.2.1.tar.gz"
-  sha256 "a55bb95ae3d3a5cbe2558a60d3f5a3ca940056138236ea6d50696a897ab01d41"
+  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v1.2.0.tar.gz"
+  sha256 "c2753b42d2e0d53e24c5a816df213f92d519ab9f78cf335bc54597ed55378bd0"
   license "Apache-2.0"
   head "https://github.com/edgee-cloud/edgee.git", branch: "main"
 

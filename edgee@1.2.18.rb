49cc02e56c301bca047b1988b8423869f333f5a7
Edgee team <tech@edgee.cloud>
2025-07-22 10:05:42 +0000

    Release v1.2.19

--- edgee@1.2.18.rb
+++ edgee@1.2.18.rb
@@ -1,8 +1,8 @@
 class Edgee < Formula
   desc "The full-stack edge platform for your edge-oriented applications."
   homepage "https://github.com/edgee-cloud/edgee"
-  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v1.2.19.tar.gz"
-  sha256 "598b55eeda37fed8a870914f07a90124085c8dbcd1c6d8be5ed36dc20a8c2c8e"
+  url "https://github.com/edgee-cloud/edgee/archive/refs/tags/v1.2.18.tar.gz"
+  sha256 "a31fc6b0a1a5ef9fc2d4bab36c91efffb2ecebeeeec52255c254ead004425ca8"
   license "Apache-2.0"
   head "https://github.com/edgee-cloud/edgee.git", branch: "main"
 
